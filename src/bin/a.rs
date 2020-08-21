a061be2f09930d0d31a6b10f64475da86a5bf23a
masa <masatoshi.sboard@gmail.com>
2020-08-22 03:47:10 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,41 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        h: usize,
+        w: usize,
+        map: [Chars; h],
+    }
+    let mut ans = true;
+    'outer: for i in 0..h {
+        if i == h - 1 {
+            for j in 0..w {
+                if j >= 1 && map[i - 1][j] == '#' && map[i][j - 1] == '#' {
+                    ans = false;
+                    break 'outer;
+                }
+            }
+            break 'outer;
+        }
+        for j in 0..w {
+            if map[i][j] == '.' {
+                continue;
+            }
+            if j == w - 1 {
+                if i >= 1 && map[i][j - 1] == '#' && map[i - 1][j] == '#' {
+                    ans = false;
+                    break 'outer;
+                }
+                continue;
+            }
+            if map[i + 1][j] == '#' && map[i][j + 1] == '#' {
+                ans = false;
+                break 'outer;
+            }
+        }
+    }
+    if ans {
+        println!("Possible");
+    } else {
+        println!("Impossible");
     }
 }
